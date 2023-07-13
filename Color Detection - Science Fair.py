ec7b26d24e391a30a9b4c46072859f5549f24ba9
Agamveer Sansoe <agamveer018@gmail.com>
2023-07-12 23:20:16 -0400

    Detect red and blue

--- Color Detection - Science Fair.py
+++ Color Detection - Science Fair.py
@@ -1,11 +1,18 @@
  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Written by Agamveer Singh Sansoe
+# Written by Agamveer Sansoe
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 import cv2
 import os
 import numpy as np
 import pandas as pd
+
+
+
+# ---------------------------------------------------------------------------------------------------------------
+# RUNNING A VIDEO CAPTURE
+# ---------------------------------------------------------------------------------------------------------------
+
 
 # Capture video feed into an object
 # Syntax: cv2.VideoCapture(number to specify which camara.. usually one camara which is 0)
@@ -32,20 +39,60 @@ while True:
         print("Cannot recieve frame (stream ended?). Exiting...")
         break
 
-    # Operations of frame
-    normal_color = cv2.cvtColor(frame,cv2.COLOR_BGR2BGRA)
+
+    # Convert colorspace of video capture
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
+
+    # Assign lower and upper blue color variables
+    # syntax: boudaries = (upper [R,B,G], lower [R,B,G])
+    # Blue color values: 120 255 255
+    light_blue = np.array([110,100,100])
+    dark_blue = np.array([130,255,255])
+
+    # Assign lower and upper red color variables
+    # Red color values: 0 255 255
+    light_red = np.array([-10, 100, 100])
+    dark_red =  np.array([10, 255, 255])
+
+    # Set color range
+    # Syntax: (Accepts three parameters) cv2.inRange(input image, lower color boundray, upper color boundary)
+    # Threshold HSV image to only get blue colors
+    blue_mask = cv2.inRange(hsv,light_blue,dark_blue)
+    red_mask = cv2.inRange(hsv,light_red,dark_red)
+
+
+    # if there are any white pixels on mask, sum will be > 0
+    has_blue = np.sum(blue_mask)
+
+    has_red = np.sum(red_mask)
+
+    # Checks if white pixels are on mask and returns string if there is
+    if has_blue > 0:
+        print("Blue Detected\n\n\n")
+    if has_red > 0:
+        print("Red Detected\n\n\n")
+
+    # cv2.inRange() reutrns a binary mask that we'll pass into the bitwise AND operator
+    blue_mask_output = cv2.bitwise_and(frame,frame,mask=blue_mask)
+    red_mask_output = cv2.bitwise_and(frame,frame,mask=red_mask)
+
 
     # Display resulting frame
-    cv2.imshow("Video Capture", normal_color)
+    cv2.imshow("Blue Color Detection", blue_mask_output)
+    cv2.imshow("Red Color Detection", red_mask_output)
 
     # Press q to quit
     if cv2.waitKey(1) == ord('q'):
         break
 
 
+# Stops video capture
 cam.release()
 cv2.destroyAllWindows()
 
+
+# ---------------------------------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------------------------------
 
