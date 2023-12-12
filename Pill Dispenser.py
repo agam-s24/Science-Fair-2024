a780b11ba5b8e7685a69fcf8bd7689ebf652f284
Agamveer Sansoe <agamveer018@gmail.com>
2023-12-12 16:54:20 -0500

    commit

--- Pill Dispenser.py
+++ Pill Dispenser.py
@@ -9,7 +9,7 @@ import os
 # Make every button a function --> when button is pressed, function is called
 # everything happens in a while loop (while [sysRunning is true], check for signal of this button [if signal is found, execute function])
 
-
+# Code a “start “ button to start dispensing process. Turn stepper motor and slow speed (in a certain direction) to slowly drop the dispenser. If motion sensor detects something, turn stepper motor fast at opposite direction (to make the dispenser go up) with certain calculated delay. Variable “x” repeat process. (Variable x determined by input)
 
 
 
@@ -17,7 +17,7 @@ import os
 sysRunning = True
 
 # Button assignment
-buttonPin = 17
+poweroff_button_Pin = 17
 
 # SETTING UP 
 
@@ -27,16 +27,16 @@ def setup():
     GPIO.setmode(GPIO.BCM)
 
     #set up pins
-    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(poweroff_button_Pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def buttonSignal():
-    inputState = GPIO.input(buttonPin)
+def poweroff_button_signal():
+    inputState = GPIO.input(poweroff_button_Pin)
     if inputState == GPIO.LOW:
         return True
     elif inputState == GPIO.HIGH:
         return False
     else:
-        print("unable to detect input on " + buttonPin)
+        print("unable to detect input on " + poweroff_button_Pin)
         return None
 
 # FUNCTIONS (BUTTONS)
@@ -45,12 +45,14 @@ def poweroff():
     print("powering off.. ") # PLACEHOLDER ACTION. Replace with actual powering off code
     sys.exit() # stop program
     
+def start():    
+    pass
 
 # Initlizae GPIO
 
 
 while sysRunning:
-    if buttonSignal() == True:
+    if poweroff_button_Pin() == True:
         poweroff()
 
     # avoid high CPU 
